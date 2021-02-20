e35c56a6238a19498b8c19d1657919653a9dc044
Omar Branez <omar.branez@gmail.com>
2021-02-20 15:51:29 -0500

    commit tree f4a7afa0cbb599e710382491e5427e6f66d8a720

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.none?(brand)
+      BRANDS << brand
+    end
+    #BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
@@ -11,4 +17,6 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+
+  
 end
